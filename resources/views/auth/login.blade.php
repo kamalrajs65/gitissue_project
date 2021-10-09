708e8e70d9f8c48d143f0945ae2ca75b042fcb29
kamal <>
2021-10-09 10:05:40 +0530

    Login and app blade updated
    Login and app blade updated

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,6 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
+            
                 <div class="card-header">{{ __('Login') }}</div>
 
                 <div class="card-body">
@@ -16,6 +17,7 @@
                             </div>
                         </div>
                 </div>
+                
             </div>
         </div>
     </div>
